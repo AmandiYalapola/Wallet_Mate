b260edd1c19580eb3b761d02907c1cde1fa92f96
IT21303470 <it21303470@my.sliit.lk>
2023-05-12 13:14:56 +0530

    edited

---
+++ app/src/main/java/com/example/wallet_mate/DashboardActivity.kt
@@ -0,0 +1,42 @@
+package com.example.wallet_mate
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wallet_mate.databinding.DashboardBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: DashboardBinding
+
+    private  lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        binding.dashgBackbtn.setOnClickListener{
+            firebaseAuth.signOut()
+            startActivity(Intent(this,MainActivity::class.java))
+           finish()
+        }
+        binding.ivincome1.setOnClickListener{
+
+            startActivity(Intent(this,IncomeActivity::class.java))
+
+        }
+    }
+
+    private fun checkUser() {
+
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser==null){
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/wallet_mate/MainActivity.kt
+++ app/src/main/java/com/example/wallet_mate/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.wallet_mate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import com.example.wallet_mate.databinding.LoginBinding
+import com.google.firebase.database.core.view.View
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding:LoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.welcome_page)
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.loginBtn.setOnClickListener{
+            startActivity(Intent(this,LoginActivity::class.java))
+
+
+        }
+        binding.googleSignInBtn.setOnClickListener{
+
+        }
+
+//        fun makeCurrentFragment(fragment: Fragment) { //method 2
+//            supportFragmentManager.beginTransaction().apply {
+//                replace(R.id.fl_wrapper, fragment)
+//                commit()
+//            }
+        }
+
+
+
     }
-}
+
